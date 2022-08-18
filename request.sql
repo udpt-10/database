df943d06c408d95d3752f184c32f9c81a2c20e81
BerNardTran <tbkhanh0908@gmail.com>
2022-08-19 00:58:42 +0700

    Create request.sql

---
+++ request.sql
@@ -0,0 +1,88 @@
+create database requestManager
+use requestManager
+create table [employee] (
+	[employee_id] int primary key,
+)
+
+create table [ot_request] (
+	[ot_request_id] int identity primary key,
+	[employee_id] int,
+	[date] date,
+	[hour] float,
+	[reason] nvarchar(500)
+)
+alter table [ot_request] add CONSTRAINT  ot_request_ FOREIGN KEY ([employee_id]) REFERENCES [employee]([employee_id])
+
+create table [approve_ot] (
+	[approve_ot_id] int identity primary key,
+	[ot_request_id] int,
+	[mamager_id] int,
+	[is_approved] bit,
+	[reason] nvarchar(500),
+	[date] date
+)
+
+alter table [approve_ot] add CONSTRAINT  approve_ot_ FOREIGN KEY ([mamager_id]) REFERENCES [employee]([employee_id])
+
+create table [work_from_home_request] (
+	[work_from_home_request_id] int identity primary key,
+	[employee_id] int,
+	[date] date,
+
+) 
+
+alter table [work_from_home_request] add CONSTRAINT  wfh_request_ FOREIGN KEY ([employee_id]) REFERENCES [employee]([employee_id])
+
+create table [leave_request] (
+	[leave_request_id] int identity primary key,
+	[employee_id] int,
+	[date] date,
+	[hour] float,
+	[reason] nvarchar(500)
+)
+alter table [leave_request] add CONSTRAINT  leave_request_ FOREIGN KEY ([employee_id]) REFERENCES [employee]([employee_id])
+
+create table [approve_leave] (
+	[approve_leave_id] int identity primary key,
+	[ot_request_id] int,
+	[mamager_id] int,
+	[is_approved] bit,
+	[reason] nvarchar(500),
+	[date] date
+)
+
+alter table [approve_leave] add CONSTRAINT  approve_leave_ FOREIGN KEY ([mamager_id]) REFERENCES [employee]([employee_id])
+
+
+create table [support_request] (
+	[support_request_id] int identity primary key,
+	[employee_id] int,
+	[date] date,
+	[department] nvarchar(500)
+)
+
+alter table [support_request] add CONSTRAINT  support_request_ FOREIGN KEY ([employee_id]) REFERENCES [employee]([employee_id])
+
+create table [approve_support] (
+	[approve_support_id] int identity primary key,
+	[approver] int,
+	[date] date,
+	[is_approved] bit,
+	[reason] nvarchar(500),
+	[director_id] int,
+	[money] money,
+	[status] nvarchar(100),
+	[describe] nvarchar(100)
+)
+
+alter table [approve_support] add CONSTRAINT  approve_support_manager_ FOREIGN KEY ([approver]) REFERENCES [employee]([employee_id])
+alter table [approve_support] add CONSTRAINT  approve_support_director_ FOREIGN KEY ([director_id]) REFERENCES [employee]([employee_id])
+
+create table [general_info] (
+	[general_info_id] int identity primary key,
+	[title] nvarchar(100),
+	[describe] nvarchar(1000),
+	[author] int,
+	[date] date,
+)
+alter table [general_info] add CONSTRAINT  general FOREIGN KEY ([author]) REFERENCES [employee]([employee_id])
